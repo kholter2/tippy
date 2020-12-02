7f4f11ee3ba07323e04bb1d32d1ffd9446782104
kholter2 <44233592+kholter2@users.noreply.github.com>
2020-12-02 14:57:51 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Kyle on 12/2/20.
+//  Copyright © 2020 Kyle Holter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
